bf4a07a533d0e58114548dea481ede93d25a3f8c
Julia Grimes <julia.s.grimes@gmail.com>
2023-05-18 09:38:00 -0400

    Destroy gameObject on laser position

--- Laser.cs
+++ Laser.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    //speed variable of 8f
+    [SerializeField]
+    private float _speed = 8f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //translate laser up
+        transform.Translate(new Vector3(0, _speed, 0) * Time.deltaTime);
+
+        //if laser position > 8 on the y
+        //destroy object
+
+        if (transform.position.y > 7.1)
+        {
+            Debug.Log("destroy");
+            Destroy(gameObject);
+
+        }
     }
 }
