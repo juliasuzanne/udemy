3241dbce7cc718ccec72ca343a91c8401866affc
Julia Grimes <julia.s.grimes@gmail.com>
2023-05-19 10:00:19 -0400

    Added behavior of moving down and starting at a random position

--- Enemy.cs
+++ Enemy.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _enemySpeed = -2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = (new Vector3(Random.Range(-7f,7f), 2, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move down 4m/s
+        //check if bottom at screen, respawn at top with a new random x position
+        transform.Translate(new Vector3(0, _enemySpeed, 0) * Time.deltaTime);
+
+
+
     }
 }
