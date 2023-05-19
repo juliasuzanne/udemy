0d6f44bd3710468f268bea0961f62fe5a229d4d3
Julia Grimes <julia.s.grimes@gmail.com>
2023-05-19 13:19:52 -0400

    Added coroutine to spawn manager. while loop inside an IEnumerator with yield statement, infinite while loop. Start coroutine in start function.

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject _enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("SpawnRoutine");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    //spawn game objects every 5 seconds
+    //create a coroutine of type IEnumerator -- Yield Events
+    //while loop
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {            
+        Vector3 posToSpawn = new Vector3(Random.Range(-7f, 7f), 7, 0);
+        Instantiate(_enemy, posToSpawn, Quaternion.identity);
+        yield return new WaitForSeconds(5f);
+        }
+        // always use infinite loops because we can use yield events
+        // instantiate referenced object, enemy prefab
+        // yield, wait for 5 seconds
+
+    }
+
 }
